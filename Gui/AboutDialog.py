88774121686c1384592f402eee68a4e47c3f927b
sashayanin <sashayanin@gmail.com>
2016-03-02 21:53:45 +0300

    version increased to 0.2.3a

--- Gui/AboutDialog.py
+++ Gui/AboutDialog.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk, GdkPixbuf
 
 # version number
 # major, minor, revision
-VERSION = "0.2.2a"
+VERSION = "0.2.3a"
 
 # about ats analyzer dialog based on Gtk.AboutDialog
 class AboutDialog(Gtk.AboutDialog):
