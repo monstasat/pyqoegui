146f34dba5edff9c4685a08ca18c2e2d8991c382
alexander <sashayanin@gmail.com>
2016-05-06 09:58:50 +0300

    unwanted print removed

--- Control/ErrorDetector/ProgramStats.py
+++ Control/ErrorDetector/ProgramStats.py
@@ -26,8 +26,6 @@ class ProgramStats():
                          'loudness': False}
 
     def eval_video(self, bufs):
-        if self.vdata_hdr == [0, 2030, 2031]:
-            print(bufs)
         for k, v in self.verr_cnts.items():
             if k == 'black':
                 v.eval((bufs[1], bufs[3]))
